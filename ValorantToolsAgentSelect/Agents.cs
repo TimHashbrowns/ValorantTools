091459ec74428d5584a802e0fa69069221adf40b
ImLosingTheGame <justjoehashbrowns@gmail.com>
2022-04-10 13:31:59 -0500

    init

---
+++ ValorantToolsAgentSelect/Agents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValorantToolsAgentSelect
+{
+	public static class Agents
+	{
+		private static Random rnd = new Random();
+
+		private static List<string> agents { get; set; } = new List<string>()
+		{ "Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Jett", "KAY/O", "Killjoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", 
+			"Sage", "Skye", "Sova", "Viper", "Yoru" };
+
+		public static string GetRandom()
+		{
+			int index = rnd.Next(agents.Count);
+
+			return agents[index];
+		}
+
+		public static string IfTaken()
+		{
+			int result = rnd.Next(2);
+
+			//return result;
+			return result == 1 ? "Next" : "Previous";
+		}
+	}
+}
--- ValorantToolsAgentSelect/Program.cs
+++ ValorantToolsAgentSelect/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using ValorantToolsAgentSelect;
+
+string agentStatement = $"Agent: { Agents.GetRandom() }";
+string ifTakenStatement = $"If taken: { Agents.IfTaken() }";
+
+Console.WriteLine(agentStatement);
+Console.WriteLine(ifTakenStatement);
